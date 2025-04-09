dca8c31e3e461f7b1a7221c5084a56970eafa8cc
Darsh-Demo <pandeysudarshan284@gmail.com>
2025-04-09 13:25:39 +0530

    add diffrent color

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -155,7 +155,7 @@ const TaskList = () => {
             </div>
             <div className="flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl ">
                 <div className="flex justify-between items-center">
-                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h3 className="bg-black px-3 py-1 rounded">High</h3>
                     <h4 className="text-sm">20 feb 2024</h4>
                 </div>
                 <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
