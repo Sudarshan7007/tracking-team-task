285890ced72057eda6e3ae6f1f0a51d72bac76dd
Darsh-Demo <pandeysudarshan284@gmail.com>
2025-04-09 13:14:36 +0530

    first commit

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,170 @@
+import React from "react";
+
+const TaskList = () => {
+    return (
+        <div id="tasklist" className="flex overflow-x-auto items-center justify-start gap-10 h-[55%] flex-nowrap w-full py-5 mt-5 ">
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl ">
+                <div className="flex justify-between">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl ">
+                <div className="flex justify-between">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl ">
+                <div className="flex justify-between">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl ">
+                <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm">20 feb 2024</h4>
+                </div>
+                <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+                <p className="text-sm mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, amet officia modi maxime eius illo iusto quasi quae esse et sequi. Provident, quod ipsum deleniti consequatur ex fuga praesentium facilis!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default TaskList
